f5cef851db225e5fb70985df83bd8a7638d47c87
Andreshk <andreshko.dre@gmail.com>
2020-10-13 14:54:00 +0300

    add ex0 code & ex1 tasks

---
+++ group-g/ex00-20201006.rkt
@@ -0,0 +1,17 @@
+; hello, world <- това е коментар
+
+(define x 5)
+
+(define (1+ x) (+ x 1))
+
+(+ 2 3 (* 2 2) (1+ 10))
+
+(define % remainder)
+
+(define (fact n)
+  (if (< n 2)
+      n
+      (* n (fact (- n 1)))))
+
+
+
