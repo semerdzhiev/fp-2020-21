fe2a9f8e42b5f84ada1877f73531cb3dbc66475e
Boyan Vushkov <bvushkov@gmail.com>
2020-12-02 15:24:31 +0200

    Update graphs task for group-b

--- group-b/class/09.graphs.rkt
+++ group-b/class/09.graphs.rkt
@@ -4,29 +4,50 @@
 ; например '((1 . 2) (2 . 3) (2 . 5) (2 . 4) (4 . 3) (5 . 4))
 
 (define g '((1 . 2) (2 . 3) (2 . 5) (2 . 4) (4 . 3) (5 . 4)))
+; '(1 2 2 3 2 5 2 4 4 3 5 4)
 
-; търсим входна степен на даден връх
+; '((2 . 3) (2 . 5) (2 . 4))
+; '(3 5 4)
+
+; търсим входна степен на даден връх - колко ребра влизат в него
 (define (in-degree g v)
   (void)
 )
 
-; търсим изходна степен на даден връх
+; търсим изходна степен на даден връх - колко ребра излизат от него
 (define (out-degree g v)
   (void)
 )
 
 ; искаме списък с всички върхове на g
 (define (nodes g)
-  (void)
+  (remove-duplicates (flatten g))
+  ;(remove-duplicates (foldr (lambda (currEdge currRes) (cons (from currEdge) (cons (to currEdge) currRes))) '() g))
+  ;(void)
 )
+; '(1 2 ((3)) (4 (5))) -> (1 2 3 4 5)
 
 ; преобразуваме g към представяне със списък на съседство
+; (
+;  (1 (2))
+;  (2 (3 4 5))
+;  (3 ())
+;  (4 (3))
+;  (5 (4))
+; )
 (define (to-adjacency-list g)
-  (void)
+  (define (neighbours v g)
+    (let ((edges-starting-with-v (filter (lambda (edge) (= (car edge) v)) g)))
+    (map cdr edges-starting-with-v))
+  )
+  (map
+   (lambda (v) (cons v (list (neighbours v g))))
+   (nodes g)
+  )
 )
 
-; проверяваме дали в g има път между два дадени върха
-(define (path? g from to)
+; искаме да проверим дали списъкът от върхове nodes е път в графа g
+(define (path? g nodes)
   (void)
 )
 
