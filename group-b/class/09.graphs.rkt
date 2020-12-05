fccbd312af6c30f822b9c8a178a2d2fee012cc97
Boyan Vushkov <bvushkov@gmail.com>
2020-12-05 10:47:26 +0200

    Update last group-b exercise

--- group-b/class/09.graphs.rkt
+++ group-b/class/09.graphs.rkt
@@ -4,28 +4,23 @@
 ; например '((1 . 2) (2 . 3) (2 . 5) (2 . 4) (4 . 3) (5 . 4))
 
 (define g '((1 . 2) (2 . 3) (2 . 5) (2 . 4) (4 . 3) (5 . 4)))
-; '(1 2 2 3 2 5 2 4 4 3 5 4)
-
-; '((2 . 3) (2 . 5) (2 . 4))
-; '(3 5 4)
 
 ; търсим входна степен на даден връх - колко ребра влизат в него
 (define (in-degree g v)
-  (void)
+  (length (filter (lambda (x) (= (cdr x) v)) g))
 )
 
 ; търсим изходна степен на даден връх - колко ребра излизат от него
 (define (out-degree g v)
-  (void)
+  (length (filter (lambda (x) (= (car x) v)) g))
 )
 
 ; искаме списък с всички върхове на g
 (define (nodes g)
   (remove-duplicates (flatten g))
   ;(remove-duplicates (foldr (lambda (currEdge currRes) (cons (from currEdge) (cons (to currEdge) currRes))) '() g))
-  ;(void)
 )
-; '(1 2 ((3)) (4 (5))) -> (1 2 3 4 5)
+; (flatten (1 2 ((3)) (4 (5))) -> (1 2 3 4 5)
 
 ; преобразуваме g към представяне със списък на съседство
 ; (
@@ -47,8 +42,33 @@
 )
 
 ; искаме да проверим дали списъкът от върхове nodes е път в графа g
+; 1 2 3 4 5
+; 1 2 3 4
+; 2 3 4 5
+; zip
+; '((1 . 2) (2 . 3) (3 . 4) (4 . 5))
 (define (path? g nodes)
-  (void)
+  (define (remove-last list)
+    (if(null? list) '() 
+       (reverse (cdr (reverse list))))
+  )
+  (define remove-first cdr)
+  ; (zip '() '(1 2 3)) -> '()
+  ; (zip '(1) '(2 3)) -> '((1 2))
+  (define (zip xs ys)
+    (if (or (null? xs) (null? ys))
+        '()
+        (cons (cons (car xs) (car ys)) (zip (cdr xs) (cdr ys)))))
+
+  (define (all? p? xs)
+    (cond ((null? xs) #t)
+          ((not (p? (car xs))) #f)
+          (else (all? p? (cdr xs)))))
+        
+
+  (let ((path-edges (zip (remove-last nodes) (remove-first nodes))))
+    (all? (lambda (path-edge) (member path-edge g)) path-edges)
+  )
 )
 
 ; искаме всички (прости) пътища между два дадени върха
